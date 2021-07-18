6add05e4f6b3a85ebc55d7b31d4b8c021eeb0b89
Guru127 <gurucharan.gcn@gmail.com>
2021-07-18 15:15:02 +0530

    messege added to show when no tasks is there

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ const deleteTask = (id) =>{
   return (
     <div className="container">
      <Header />
-     <Tasks tasks={tasks} onDelete = {deleteTask}/>
+     {tasks.length>0? <Tasks tasks={tasks}
+      onDelete = {deleteTask}/>:'No Tasks To Show'}
     </div>
   )
 }
