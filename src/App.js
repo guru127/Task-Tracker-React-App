9f394ccaaf0ee8be947c03ae57546110a9638871
Guru127 <gurucharan.gcn@gmail.com>
2021-07-18 11:24:38 +0530

    class bassed component rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
+import React from "react";
 import Header from "./component/Header";
 
-const App =() => {
-  return (
-    <div className="container">
-    <Header title='Hello'/>
-    </div>
-  )
+// const App =() => {
+//   return (
+//     <div className="container">
+//     <Header title='Hello'/>
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+ render() {
+    return<div>
+      <Header/>
+      <h1>Hii Guru</h1>
+
+    </div> 
+    
+ }
 }
 
 export default App;
